45d1140877960db8948c8feba72310412d74bd07
Kip Cole <kipcole9@gmail.com>
2014-09-16 21:27:15 +0900

    Ready for Rails 4.2

--- lib/postgresql/inheritance/postgres_oid.rb
+++ lib/postgresql/inheritance/postgres_oid.rb
@@ -1,74 +1,42 @@
 ActiveSupport.on_load(:active_record) do
   module ActiveRecord
-    module ConnectionAdapters
-      class PostgreSQLAdapter < AbstractAdapter
-        module OID
-          # TODO:  Rather than use a serializer, here may be a better place to
-          # do casting of geo data to RGeo types
-          class Geometry < Type
-            def type_cast(value)
-              return if value.nil?
-              value
-              # ConnectionAdapters::PostgreSQLColumn.string_to_rgeo value
-            end
-          end
+    module Type
+      class Geometry < Value
+        include Mutable
         
-          class Geography < Type
-            def type_cast(value)
-              return if value.nil?
-              value
-              # ConnectionAdapters::PostgreSQLColumn.string_to_rgeo value
-            end
-          end
-          
-          class Point < Type
-            def type_cast(value)
-              return if value.nil?
-              value
-              # ConnectionAdapters::PostgreSQLColumn.string_to_rgeo value
-            end
-          end
-          
-          class Varbit < Type
-            def type; :varbit; end
-            
-            def type_cast(value)
-              return if value.nil?
-              value
-            end
-          end
+        FACTORY = RGeo::Geographic.simple_mercator_factory(
+          :wkb_parser => {:support_ewkb => true}, :wkb_generator => {:type_format => :ewkb, :hex_format => true, :emit_ewkb_srid => true})
+        PROJECTION_FACTORY = FACTORY.projection_factory
+        
+        def type
+          :geometry
+        end
+
+        def type_cast_from_database(value)
+          value ? FACTORY.unproject(PROJECTION_FACTORY.parse_wkb(geo)) : nil
+        end
+
+        def type_cast_for_database(value)
+          value ? FACTORY.project(value).as_binary.unpack('H*').first : nil
         end
       end
-    end
-  end
+    
+      class Geography < Value
+        include Mutable
+        
+        FACTORY = RGeo::Geographic.spherical_factory(
+          :wkb_parser => {:support_ewkb => true}, :wkb_generator => {:hex_format => true, :type_format => :ewkb, :emit_ewkb_srid => true})
+        
+        def type
+          :geography
+        end
 
-  module ActiveRecord
-    module ConnectionAdapters
-      # PostgreSQL-specific extensions to column definitions in a table.
-      class PostgreSQLColumn < Column #:nodoc:
+        def type_cast_from_database(value)
+          value ? FACTORY.parse_wkb(value) : nil
+        end
 
-        private
-
-        # Maps PostgreSQL-specific data types to logical Rails types.
-        # Used by schema_dumper to decide what column type to generate
-        alias :simplified_type_without_geo :simplified_type
-        def simplified_type(field_type)
-          case field_type
-          when 'geometry'
-            :geometry
-          when 'geography'
-            :geography
-          when 'point'
-            :point
-          when /bit/i
-            :varbit
-          else
-            if ActiveRecord::Base.connection.enum_types.include?(field_type)
-              :string
-            else
-              simplified_type_without_geo(field_type)
-            end
-          end
+        def type_cast_for_database(value)
+          value ? value.as_binary : nil
         end
       end
     end
@@ -96,41 +64,20 @@ ActiveSupport.on_load(:active_record) do
               point(name, options)
             end             
           end
-          
-          def point(name, options = {})
-            if ActiveRecord::Base.connection.extensions.include? 'postgis'
-              geography(name, options)
-            else
-              column(name, 'point', options)
-            end
-          end
-          
-          def varbit(name, options = {})
-            column(name, 'varbit', options)
-          end
         end
       
         base_types = NATIVE_DATABASE_TYPES.dup
-        geo_types = {geography: {name: 'geography'}, geometry: {name: 'geometry'}, point: {name: 'point'}}
-        other_types = {varbit: {name: 'varbit'}}
+        geo_types = {geography: {name: 'geography'}, geometry: {name: 'geometry'}}
         self.send(:remove_const, :NATIVE_DATABASE_TYPES)
-        self.const_set(:NATIVE_DATABASE_TYPES, base_types.merge(geo_types).merge(other_types))
+        self.const_set(:NATIVE_DATABASE_TYPES, base_types.merge(geo_types))
       end
     end
   end
 
   # Register additional data types for Postgres so we don't get a console warning. 
-  ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::OID.instance_eval do
-    register_type 'geography',  ::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::OID::Geography.new
-    register_type 'geometry',   ::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::OID::Geometry.new
-    register_type 'point',      ::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::OID::Point.new
-    register_type 'varbit',     ::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::OID::Varbit.new
-    alias_type 'regclass',      'text'
-
-    # Register enum types as well
-    ActiveRecord::Base.connection.enum_types.each do |enum|
-      register_type enum, ::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::OID::Identity.new
-    end
+  ActiveRecord::Base.connection.tap do |conn|
+    conn.type_map.register_type 'geography',  ::ActiveRecord::Type::Geography.new
+    conn.type_map.register_type 'geometry',   ::ActiveRecord::Type::Geometry.new
+    conn.type_map.alias_type 'regclass',      'text'
   end
-  ActiveRecord::Base.connection.send(:reload_type_map) if ActiveRecord::Base.connected?
 end
