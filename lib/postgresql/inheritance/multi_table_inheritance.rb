6714625affac935d50ced265e260ff8b62c56bec
Kip Cole <kipcole9@gmail.com>
2015-02-23 15:31:32 +0900

    Move to Rails 4.2.1

--- lib/postgresql/inheritance/multi_table_inheritance.rb
+++ lib/postgresql/inheritance/multi_table_inheritance.rb
@@ -6,10 +6,9 @@ module ActiveRecord
   
   private
     # Retrieve the OID as well on a default select
-    def build_select(arel, selects)
-      unless selects.empty?
-        @implicit_readonly = false
-        arel.project(*selects)
+    def build_select(arel)
+      if select_values.any?
+        arel.project(*arel_columns(select_values.uniq))
       else
         arel.project("\"#{klass.table_name}\".\"tableoid\"::regclass as \"type\"") if @klass.using_multi_table_inheritance?
         arel.project(@klass.arel_table[Arel.star])
